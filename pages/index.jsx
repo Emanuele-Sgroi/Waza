4beb390130b66e1f2f6752c613be0b1b9a94f1ee
Amrod92 <manlio92--@live.it>
2023-03-07 17:08:12 +0000

    Testing disable website for mobile

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,14 @@
 import LandingPage from '../components/LandingPage';
+import { useRouter } from 'next/router';
+import { isMobileDevice } from '../utils/device';
 
 export default function Home() {
+  const router = useRouter();
+
+  if (isMobileDevice(router.req)) {
+    return <div>Sorry, this website is not available on mobile devices.</div>;
+  }
+
   return (
     <>
       <div className='gradient'>
---
+++ utils/device.js
@@ -0,0 +1,10 @@
+import UAParser from 'ua-parser-js';
+
+export function isMobileDevice(req) {
+  if (typeof navigator !== 'undefined') {
+    const parser = new UAParser();
+    const userAgent = req ? req.headers['user-agent'] : navigator.userAgent;
+    const result = parser.setUA(userAgent).getResult();
+    return result.device && result.device.type === 'mobile';
+  }
+}
