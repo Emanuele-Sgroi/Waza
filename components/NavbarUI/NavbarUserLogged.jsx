4aef9d379adbc057e45e84f19dee48e50dcef7db
Amrod92 <manlio92--@live.it>
2023-03-24 09:08:08 +0000

    Fix search ring color

--- components/NavbarUI/NavbarUserLogged.jsx
+++ components/NavbarUI/NavbarUserLogged.jsx
@@ -11,13 +11,31 @@ const NavbarUserLogged = ({ session }) => {
         <Menu as='div' className='relative inline-block text-left'>
           <div>
             <Menu.Button className='inline-flex w-full justify-center bg-opacity-20 text-sm font-medium text-white hover:bg-opacity-100 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black'>
-              <Image
-                width={50}
-                height={50}
-                className='rounded-full cursor-pointer'
-                src={session.user?.image}
-                alt='Profile Photo'
-              />
+              {session.user?.image ? (
+                <Image
+                  width={50}
+                  height={50}
+                  className='rounded-full cursor-pointer'
+                  src={session.user?.image}
+                  alt='Profile Photo'
+                />
+              ) : (
+                <div class='flex items-center mt-4 space-x-3'>
+                  <svg
+                    class='text-gray-200 w-14 h-14 dark:text-gray-700'
+                    aria-hidden='true'
+                    fill='currentColor'
+                    viewBox='0 0 20 20'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      fill-rule='evenodd'
+                      d='M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z'
+                      clip-rule='evenodd'
+                    ></path>
+                  </svg>
+                </div>
+              )}
             </Menu.Button>
           </div>
 
--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -128,7 +128,7 @@ const ProjectPage = () => {
                       id='default-search'
                       value={search}
                       onChange={e => setSearch(e.target.value)}
-                      className='block w-full p-4 pl-10 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-white-500 focus:border-white-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-white-500 dark:focus:border-red-500'
+                      className='block w-full p-4 pl-10 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-white-500 dark:focus:border-red-500 focus:ring-red-500 focus:border-red-200'
                       placeholder='Search a title of the project...'
                       required
                     />
